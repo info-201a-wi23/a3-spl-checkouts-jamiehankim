9fc6f7a255037b70c7adbdcc91faf34e69583b19
jamiehankim <122043787+jamiehankim@users.noreply.github.com>
2023-03-02 00:50:34 -0800

    Update on chart 1

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,12 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+# Load DPLYR and ggplot
+library(dplyr)
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+# Load data 
+spl_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# Chart 1
+chart1_df <- spl_df %>% filter(CheckoutYear == "2022") %>% filter(MaterialType == "ATLAS" | MaterialType == "AUDIOBOOK" | MaterialType == "BOOK" | MaterialType == "EBOOK" | MaterialType == "SOUNDDISC") %>% select(MaterialType, CheckoutMonth, Checkouts)
+
+# Create a bar chart
+ggplot(chart1_df) + geom_bar(aes(x = CheckoutMonth, fill = MaterialType)) + labs(title = "The Five Types of Books Over Month", x = "Month(s)", y = "Number of Checkouts", fill = "MaterialType") + scale_x_continuous(breaks = seq(1, 12, 1)) 
