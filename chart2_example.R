3da3e35d16d33f78b8e71bade9ed38e5723c1515
jamiehankim <122043787+jamiehankim@users.noreply.github.com>
2023-03-02 00:50:04 -0800

    updates on chart 2

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,19 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+# Load libraries
+library("dplyr")
+library("scales")
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+# Load data 
+spl_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# Chart 2
+chart2_df <- spl_df %>% filter(MaterialType == "ATLAS" | MaterialType == "AUDIOBOOK" | MaterialType == "BOOK" | MaterialType == "EBOOK" | MaterialType == "SOUNDDISC") %>% 
+  group_by(CheckoutYear) %>% 
+  summarize(total = sum(Checkouts, na.rm = TRUE))
+
+ggplot(chart2_df) + 
+  geom_line(aes(x = CheckoutYear, 
+                y = total, 
+                color = total)) +
+  labs(title = "Trends of Checkouts From 2022 to 2023", x = "Year", y = "Number of Checkouts") + 
+  scale_y_continuous(labels = label_number_si())
